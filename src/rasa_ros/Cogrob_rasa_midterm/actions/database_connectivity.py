c923d477c5d7db9afc106453939d23927a881419
alcatraz <f.sica24@studenti.unisa.it>
2022-12-04 21:07:43 +0100

    mario

--- src/rasa_ros/Cogrob_rasa_midterm/actions/database_connectivity.py
+++ src/rasa_ros/Cogrob_rasa_midterm/actions/database_connectivity.py
@@ -160,16 +160,17 @@ class Database:
     rows = cur.fetchall()
     if(len(rows)>0):
       category = ["Activities","Category","DeadLine","Completed"]
-      number = [i for i in range(1,len(rows)+1)]
-      row_format ="{:>20}" * (len(category) + 1)
-      toPrint = ""
-      toPrint += (row_format.format("", *category)) + "\n"
-      for team, row in zip(number, rows):
-          if (row[2]):
-            row = (row[0],row[1],row[2][:10]+ " "+row[2][11:16],row[3])
-          else:
-            row = (row[0],row[1],"None",row[3])
-          toPrint += (row_format.format(team, *row)) + "\n"
+      # number = [i for i in range(1,len(rows)+1)]
+      # row_format ="{:>20}" * (len(category) + 1)
+      # toPrint = ""
+      # toPrint += (row_format.format("", *category)) + "\n"
+      # for team, row in zip(number, rows):
+      #     if (row[2]):
+      #       row = (row[0],row[1],row[2][:10]+ " "+row[2][11:16],row[3])
+      #     else:
+      #       row = (row[0],row[1],"None",row[3])
+      #     toPrint += (row_format.format(team, *row)) + "\n"
+      toPrint = rows
 
 
     return toPrint if len(rows) > 0 else None
