def3cb8308d9382b638643481a21036b223dfa2c
alcatraz <f.sica24@studenti.unisa.it>
2022-12-11 17:36:48 +0100

    added face_recognition registration, modifies name of json for test purposes

--- src/facial_emotion_recognition/src/face_reidentification_node.py
+++ src/facial_emotion_recognition/src/face_reidentification_node.py
@@ -21,8 +21,10 @@ class NumpyArrayEncoder(JSONEncoder):
         return JSONEncoder.default(self, obj)
 
 
-def save_identities(dataset, path):
-    to_save = {'dataset':dataset}
+def save_identities(path):
+    global database
+    global labels
+    to_save = {'dataset':database, 'labels': labels}
     print("sono in save")
     with open(path + '/json_data.json','w') as out_file:
         json.dump(to_save,out_file,cls=NumpyArrayEncoder)
@@ -30,6 +32,7 @@ def save_identities(dataset, path):
 
 def load_identities(path):
     print("sono in load")
+    global database, labels
     try:
         with open(path + '/json_data.json','r') as in_file:
             tmp= json.load(in_file)
@@ -47,7 +50,13 @@ pub = rospy.Publisher('identity', Detection2DArray, queue_size=2)
 face_reco_model = VGGFace(model='resnet50', include_top=False, pooling='avg')
 padding = 0.2
 INPUT_SIZE = (224,224)
+global database 
+global labels
+global actual_id
 database,labels = load_identities(REF_PATH)
+actual_id = 0
+if len(labels):
+    actual_id = max(labels) + 1
 
 def batch_cosine_similarity(x1, x2):
     '''
@@ -106,39 +115,72 @@ def extract_features(face_reco_model, filename):
     faceim = preprocess_input([faceim.astype(np.float32)], version=2)
     feature_vector = (face_reco_model.predict(faceim)).flatten()
     return feature_vector
+from datetime import datetime
 
-def predict_identity(frame,rejection_threshold=0.5):
-    feature_vector = extract_features(face_reco_model, frame).reshape(-1,2048)
-    emb_face = np.repeat(feature_vector, len(database), 0).reshape(-1,2048)
-    cos_dist = batch_cosine_similarity(np.array(database), emb_face)
-    id_label = dist2id(cos_dist, labels, rejection_threshold, mode='avg')
-    return int(id_label)
+def registration():
+    t1 = datetime.now()
+    print('non ti conosco... Rimani fermo ')
+    for _ in range(10):
+        msg = rospy.wait_for_message('face_reidentification',Detection2DArray)
+        im = ros_numpy.numpify(msg.detections[0].source_img)
+        for d in msg.detections:
+            # Preprocess image
+            d,resized_face = elaboration(d,im)
+            feature_vector = extract_features(face_reco_model, resized_face)
+            database.append(feature_vector)
+            labels.append(actual_id)
+    # Predict
+    t2 = datetime.now()
+    print('la procedura di registrazione ha impiegato' ,(t2-t1))
+
+def elaboration(d,im):
+    bbox_0 = d.bbox.center.y - d.bbox.size_y/2
+    bbox_1 = d.bbox.center.x - d.bbox.size_x/2
+    bbox_2 = d.bbox.size_x + bbox_0
+    bbox_3 = d.bbox.size_y + bbox_1
+    padding_px = int(padding*max(d.bbox.size_y,d.bbox.size_x))
+    face = im[round(max(0,bbox_1-padding_px)):round(min(bbox_3+padding_px,im.shape[0]-1)),round(max(0,bbox_0-padding_px)):round(min(bbox_2+padding_px, im.shape[1]-1))]
+    face = face[ face.shape[0]//2 - face.shape[1]//2 : face.shape[0]//2 + face.shape[1]//2, :, :]
+    resized_face = cv2.resize(face,INPUT_SIZE)
+    return d,resized_face
+
+
+def predict_identity(resized_face,rejection_threshold=0.5):
+    global actual_id
+    if len(database) > 0 :
+        feature_vector = extract_features(face_reco_model, resized_face).reshape(-1,2048)
+        emb_face = np.repeat(feature_vector, len(database), 0).reshape(-1,2048)
+        cos_dist = batch_cosine_similarity(np.array(database), emb_face)
+        id_label = dist2id(cos_dist, labels, rejection_threshold, mode='avg')
+    else:
+        registration()
+        id_label = actual_id
+    return (id_label)
 
 def face_reidentification(msg):
     im = ros_numpy.numpify(msg.detections[0].source_img)
     for d in msg.detections:
-        bbox_0 = d.bbox.center.y - d.bbox.size_y/2
-        bbox_1 = d.bbox.center.x - d.bbox.size_x/2
-        bbox_2 = d.bbox.size_x + bbox_0
-        bbox_3 = d.bbox.size_y + bbox_1
-        padding_px = int(padding*max(d.bbox.size_y,d.bbox.size_x))
-        face = im[round(max(0,bbox_1-padding_px)):round(min(bbox_3+padding_px,im.shape[0]-1)),round(max(0,bbox_0-padding_px)):round(min(bbox_2+padding_px, im.shape[1]-1))]
-        face = face[ face.shape[0]//2 - face.shape[1]//2 : face.shape[0]//2 + face.shape[1]//2, :, :]
         # Preprocess image
-        resized_face = cv2.resize(face,INPUT_SIZE)
+        d,resized_face = elaboration(d,im)
         # Predict
         o = ObjectHypothesisWithPose()
         o.id = predict_identity(resized_face)
         d.results.append(o)
-
-    pub.publish(msg)
+    return msg
 
    
+def recognize():
+    try:
+        while not rospy.is_shutdown():
+            msg = rospy.wait_for_message('face_reidentification',Detection2DArray)
+            to_publish = face_reidentification(msg)
+            pub.publish(to_publish)
 
-si = rospy.Subscriber("face_reidentification", Detection2DArray, face_reidentification)
+    except rospy.exceptions.ROSInterruptException:
+        print("vado in close")
+        # save_identities()
 
-try:
-    rospy.spin()
 
-except KeyboardInterrupt:
-    print("Shutting down")
+if __name__ == '__main__':
+    recognize()
+
