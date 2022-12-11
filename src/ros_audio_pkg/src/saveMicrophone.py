65243e0ffe28d5848a29a502b57f1793770f0a03
alcatraz <f.sica24@studenti.unisa.it>
2022-12-11 18:36:11 +0100

    added launch for save audio from micrphoone

---
+++ src/ros_audio_pkg/src/saveMicrophone.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import rospy
+from std_msgs.msg import Int16MultiArray, String
+import numpy as np
+
+from speech_recognition import AudioData
+import speech_recognition as sr
+from ros_audio_pkg.msg import AudioAndText
+import os
+#from datetime import datetime
+
+# Initialize a Recognizer
+r = sr.Recognizer()
+
+# Init node
+rospy.init_node('speech_recognition', anonymous=True)
+pub1 = rospy.Publisher('AudioAndText', AudioAndText, queue_size=10)
+count = 0
+# this is called from the background thread
+def callback(audio):
+    data = np.array(audio.data,dtype=np.int16)
+    audio_data = AudioData(data.tobytes(), 16000, 2)
+    print('salvo file audio')
+    with open(os.path.dirname(__file__)+"/../audioSample/"+str(count)+"_audio_file.wav" , 'wb') as file:
+        file.write(audio_data.get_wav_data())
+    print('salvato!!')
+
+
+
+def listener():
+    rospy.Subscriber("mic_data", Int16MultiArray, callback)
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
