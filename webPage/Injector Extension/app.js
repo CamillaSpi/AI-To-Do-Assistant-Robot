acd254047c88425defca1374751c49d20524232e
MattiaMarseglia <m.marseglia1@studenti.unisa.it>
2023-01-10 09:20:58 +0100

    changes in web app

--- webPage/Injector Extension/app.js
+++ webPage/Injector Extension/app.js
@@ -11,4 +11,18 @@ sleep(5000).then(function()
     vediamo.click();
 });
 
+sleep(10000).then(function()
+{
+    // var vediamo = document.getElementById("refresh");
+    // vediamo.click();
+    var vediamo = document.getElementById("clickMe");
+    vediamo.click();
+});
 
+sleep(15000).then(function()
+{
+    // var vediamo = document.getElementById("refresh");
+    // vediamo.click();
+    var vediamo = document.getElementById("clickMe");
+    vediamo.click();
+});
