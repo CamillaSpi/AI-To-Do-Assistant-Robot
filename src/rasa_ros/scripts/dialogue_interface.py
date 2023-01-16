a061221ee090ed3d539ffc6998ada33322a06323
alcatraz <f.sica24@studenti.unisa.it>
2023-01-16 20:44:47 +0100

    asd

--- src/rasa_ros/scripts/dialogue_interface.py
+++ src/rasa_ros/scripts/dialogue_interface.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 """
+Subsequently this node waits for messages on "text2answer" topic, it forwards these messages, 
+always through the “dialogue_server” service, to the “reminder_server” node that talks with RASA server. 
+If the communication does not work, it returns an error message.
 
+This script also incude a test function that sends a series of messages to a ROS service called 'dialogue_server' 
+using the RecognizedSpoke message type. Each message is sent with an associated ID of 5. 
+The messages are a series of text commands related to scheduling and managing activities, 
+such as adding or removing activities, modifying categories, and setting reminders. 
+The function also includes a sleep function between each message to allow the service to 
+process the previous message before sending the next one. This script is likely used to test the functionality of the 'dialogue_server' service.
 """
 import rospy
 from std_msgs.msg import String
@@ -14,7 +23,7 @@ import time
 dialogue_service = rospy.ServiceProxy('dialogue_server', Dialogue)
 def testFunction():
     message = RecognizedSpoke()  
-    message.msg = "Hi i am Vito"
+    message.msg = "Ciao sono Vito"
     message.id = 5
     dialogue_service(message.msg,message.id) 
     time.sleep(0.5)
@@ -24,7 +33,7 @@ def testFunction():
     message.msg = "no"
     dialogue_service(message.msg,message.id) 
     time.sleep(0.5)
-    message.msg = "modifica il nome dell'attivita correre in palestra con il nome camminare"
+    message.msg = "modifica il nome dell'attivita correre nella categoria palestra con il nome camminare"
     dialogue_service(message.msg,message.id) 
     time.sleep(0.5)
     message.msg = "no"
@@ -42,10 +51,10 @@ def testFunction():
     message.msg = "no"
     dialogue_service(message.msg,message.id) 
     time.sleep(0.5)
-    message.msg = "aggiungi la categoria personale"
+    message.msg = "aggiungi la categoria benessere"
     dialogue_service(message.msg,message.id) 
     time.sleep(0.5)
-    message.msg = "modifica la categoria personale nella categoria universita"
+    message.msg = "modifica la categoria benessere con il nome universita"
     dialogue_service(message.msg,message.id) 
     time.sleep(0.5)
     message.msg = "si"
@@ -80,13 +89,22 @@ def testFunction():
     message.msg = "si"
     dialogue_service(message.msg,message.id) 
     time.sleep(0.5)
-    message.msg = "aggiungi studiare in personale per oggi"
+    message.msg = "aggiungi studiare in personale oggi"
     dialogue_service(message.msg,message.id) 
     time.sleep(0.5)
     message.msg = "no"
     dialogue_service(message.msg,message.id) 
     time.sleep(0.5)
-    message.msg = "modifica il nome dell'attivita chiamata studiare in personale con il nome giocare"
+    message.msg = "modifica il nome di una attivita"
+    dialogue_service(message.msg,message.id) 
+    time.sleep(0.5)
+    message.msg = "personale"
+    dialogue_service(message.msg,message.id) 
+    time.sleep(0.5)
+    message.msg = "studiare"
+    dialogue_service(message.msg,message.id) 
+    time.sleep(0.5)
+    message.msg = "giocare"
     dialogue_service(message.msg,message.id) 
     time.sleep(0.5)
     message.msg = "si"
